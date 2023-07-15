f69ba68d323aabac870b6d97047335d57cf60c68
Babimig23 <131807007+BarbaraMig@users.noreply.github.com>
2023-07-14 23:45:41 -0300

    Update quiz.js

--- assets/scripts/quiz.js
+++ assets/scripts/quiz.js
@@ -36,11 +36,11 @@ function endgame(){
     if (players['nome'][1] != '')
         if (players['nome'][1] == players['nome'][0])
             if (players['points'][1] == players['points'][0])
-                content.innerHTML += `<span id="befplay">Você tirou a mesma porcentagem no seu último jogo</span><p>${5 * players['points'][1]}% de acerto</p>`
+                content.innerHTML += `<span calss="befplay">Você tirou a mesma porcentagem no seu último jogo</span><p>${5 * players['points'][1]}% de acerto</p>`
             else
-                players['points'][1] > players['points'][0] ? content.innerHTML += `<span id="befplay">Você diminuiu seus acertos</span><p>${5 * players['points'][1]}% de acerto na última jogada</p>` : content.innerHTML += `<span id="befplay">Você aumentou seus acertos</span><p>${5 * players['points'][1]}% de acerto na última jogada</p>`
+                players['points'][1] > players['points'][0] ? content.innerHTML += `<span class="befplay">Você diminuiu seus acertos</span><p>${5 * players['points'][1]}% de acerto na última jogada</p>` : content.innerHTML += `<span id="befplay">Você aumentou seus acertos</span><p>${5 * players['points'][1]}% de acerto na última jogada</p>`
         else
-            content.innerHTML += `<span id="befplay">Último jogador: ${players['nome'][1]}</span><p>${5 * players['points'][1]}% de acerto</p>`
+            content.innerHTML += `<span class="befplay">Último jogador: ${players['nome'][1]}</span><p>${5 * players['points'][1]}% de acerto</p>`
 
     content.innerHTML += `<input id="username" type="text" placeholder="Nome" maxlength="15" autocomplete="off">
     <span id="error-name"></span>
